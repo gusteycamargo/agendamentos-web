e604d9c4971f4c92b3aceb872dc10f2ed30955e0
gustaacamargo <camargo25.gustavo@gmail.com>
2020-04-04 14:30:55 -0300

    Atualiza módulos yarn

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3333"
+  baseURL: "https://backend-agendamentos.herokuapp.com"
 });
 
 api.interceptors.request.use(async config => {
