e7de12ec41d539ee3bdefcb11bed7bff0dbf63d0
Gustavo <camargo25.gustavo@gmail.com>
2020-12-28 22:39:37 -0300

    Adiciona criptografia ao redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,22 @@
 import { createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { encryptTransform } from 'redux-persist-transform-encrypt';
 
 import rootReducers from './reducers'
 
 const persistConfig = {
   key: 'root',
   storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_KEY_CRP,
+      onError: function (error) {
+        console.log(error);
+        // Handle the error.
+      },
+    }),
+  ],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducers);
