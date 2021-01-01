4f1f3f5eef86e927c2d216aa41341cc630fccee3
Gustavo <camargo25.gustavo@gmail.com>
2021-01-01 13:19:15 -0300

    Adiciona novo tamanho a tabela de campus

--- src/pages/Campus/View Campus/index.js
+++ src/pages/Campus/View Campus/index.js
@@ -8,6 +8,7 @@ import 'react-activity/lib/Spinner/Spinner.css';
 import Bounce from 'react-activity/lib/Bounce';
 import 'react-activity/lib/Bounce/Bounce.css';
 import { useSelector } from 'react-redux';
+import TabWidth from '../../../components/TabWidth';
 
 function ViewCampus({ history }) {    
     const [campuses, setCampuses] = useState([]);
@@ -50,25 +51,27 @@ function ViewCampus({ history }) {
                         </div>
                     }
                     <div className="container-index">
-                        <table className="table table-bordered table-hover">
-                            <thead className="thead-dark">
-                                <tr>
-                                    <th scope="col">Cidade</th>
-                                    <th scope="col">Endereço</th>
-                                    <th scope="col">Status</th>
-                                </tr>
-                            </thead>                               
-                            <tbody>
-                                {campuses.map(campus => (
-                                    <tr key={campus.id}>
-                                        <td><p>{campus.city}</p></td>
-                                        <td><p>{campus.adress}</p></td>
-                                        <td><p>{campus.status}</p></td>
+                        <TabWidth>
+                            <table className="table table-bordered table-hover">
+                                <thead className="thead-dark">
+                                    <tr>
+                                        <th scope="col">Cidade</th>
+                                        <th scope="col">Endereço</th>
+                                        <th scope="col">Status</th>
                                     </tr>
-                                ))} 
-                                
-                            </tbody>
-                        </table>
+                                </thead>                               
+                                <tbody>
+                                    {campuses.map(campus => (
+                                        <tr key={campus.id}>
+                                            <td><p>{campus.city}</p></td>
+                                            <td><p>{campus.adress}</p></td>
+                                            <td><p>{campus.status}</p></td>
+                                        </tr>
+                                    ))} 
+                                    
+                                </tbody>
+                            </table>
+                        </TabWidth>
                         {(campuses.length <= 0) && 
                             <div className="zero">
                                 <p>Nada a ser exibido</p>
